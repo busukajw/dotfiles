a3a33887f0b7184c5b1d48e0b740edb26074cd3b
Aaron Walker <busukajw@gmail.com>
2016-08-29 20:33:01 +0200

    basic working dotfiles with install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#Initial install of Dotfiles
+# Assumes that MacPorts is installed and you have installed coreutils
+actual_path=$(greadlink -f "${BASH_SOURCE[0]}")
+DOTFILES_DIR=$(dirname "$actual_path")
+
+#install the initial symlinks
+
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
